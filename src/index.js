84ba88fa489a0ec63b137f68b5a0a50d45f485ac
masahiro96848 <feetwcpic9436@gmail.com>
2022-06-25 06:13:35 +0900

    タスクを追加する処理を追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-import './style.css';
+const onClickAdd = () => {
+    // テキストボックスの値を取得し、初期化する。
+    const inputText = document.getElementById('add-text').value;
+    document.getElementById('add-text').value = ""
+
+    // div生成
+    const div = document.createElement('div');
+    div.className = 'list-row';
+
+    // liタグを生成
+    const li = document.createElement('li');
+    li.innerText = inputText;
+
+    // divタグの子要素に各要素を設定
+    div.appendChild(li);
+    
+    // 未完了リストに追加
+    document.getElementById('incomplete-list').appendChild(div);
+    
+}
+
+// タスクを追加する処理
+document.getElementById('add-button').addEventListener("click", () => onClickAdd());
